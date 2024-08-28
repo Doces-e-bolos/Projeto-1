5735bc190214d935f8fdc8fd5bbea775529821ef
JulianaAparecidaVecchi <163355132+JulianaAparecidaVecchi@users.noreply.github.com>
2024-08-28 15:57:32 -0300

    Form Contato validado

--- validacaoContato.js
+++ validacaoContato.js
@@ -3,17 +3,36 @@ const TELEFONE = document.getElementById('itel')
 const EMAIL = document.getElementById('iemail')
 const MENSAGEM = document.getElementById('imsg')
 
+function validaNome(nome){
+    regex = /^[a-zA-Z\s]+$/
+    return regex.test(nome)
+}
+
+function validaTel(tel){
+    regex = /^\(?[0-9]{2}\)?[0-9]{5}-?[0-9]{4}$/
+    return regex.test(tel)
+}
+
+function validaEmail(email){
+    regex = /^[^/s]+@[^/s]+\.[^/s]+$/
+    return regex.test(email)
+}
+
 function btnEnviarOnclick(){
     if (NOME.value.length == 0){
         alert('Preenchimento de campo: Campo nome vazio')
-        NOME.focus()  
+        NOME.focus()
+    }
+    else if (!validaNome(NOME.value)){
+        alert("Dado inválido: coloque um nome válido")
+        NOME.focus()
     }
     else if (TELEFONE.value.length == 0){
         alert('Preenchimento de campo: Campo telefone vazio')
         TELEFONE.focus() 
     }
-    else if (isNaN(TELEFONE.value)){
-        alert('Dado inválido')
+    else if (!validaTel(TELEFONE.value)){
+        alert('Dado inválido : coloque um telefone válido')
         TELEFONE.value = ""
         TELEFONE.focus()
     }
@@ -21,8 +40,12 @@ function btnEnviarOnclick(){
         alert('Preenchimento de campo: Campo email vazio')
         EMAIL.focus()
     }
+    else if(!validaEmail(EMAIL.value)){
+        alert('Dado inválido : coloque um email válido')
+        EMAIL.focus()
+    }
     else if (MENSAGEM.value.length == 0){
-        alert('Preenchimento de campo: Campo mensagem vazio')
+        alert('Preencha o campo mensagem!')
         MENSAGEM.focus()
     }
     else {
